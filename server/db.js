cbc2ee0fcca9bbe6d61ae79c7b39aa47c8cc5e49
Qi <wwqq03@gmail.com>
2014-10-15 15:41:53 +0200

    Add articles model

--- server/db.js
+++ server/db.js
@@ -1,3 +1,16 @@
 var mongoose = require('mongoose');
 
 mongoose.connect("mongodb://article_reader:article_reader@ds063879.mongolab.com:63879/heroku_app29725784");
+
+var Articles = mongoose.model('articles', {
+    _id: String,
+    content: String,
+    createTime: Date,
+    drFrontId: String,
+    imgUrl: String,
+    link: String,
+    type: String,
+    updateTime: Date
+});
+
+module.exports.ARTICLES = Articles;
