5ee1199ce94fa499bf57642840269ab011dc0be2
Qi <wwqq03@gmail.com>
2014-10-15 13:57:16 +0200

    Clean backend to make it as pure REST api

--- server/server.js
+++ server/server.js
@@ -11,9 +11,15 @@ var news = require('./routes/news');
 
 var app = express();
 
+var debug = require('debug')('server');
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'));
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+/*app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');*/
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -40,7 +46,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
-        res.render('error', {
+        res.send({
             message: err.message,
             error: err
         });
@@ -51,7 +57,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.send({
         message: err.message,
         error: {}
     });
