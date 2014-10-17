2831f58f76561f8422e24e6bfdbeb4392c410fa9
Qi <wwqq03@gmail.com>
2014-10-17 16:16:14 +0200

    Show all the articles on articles' view

--- app/scripts/controllers/articles.js
+++ app/scripts/controllers/articles.js
@@ -8,15 +8,29 @@
  * Controller of the articleApp
  */
 angular.module('articleControllers')
-.controller('ArticlesCtrl', function ($scope, articlesSvc) {
+.controller('ArticlesCtrl', function ($scope, articlesSvc, $window) {
+    var userRole = $window.sessionStorage.userRole;
+    if(userRole === 'admin') {
+        $scope.isAdmin = true;
+    }
     articlesSvc.get()
     .then(
         function(data) {
+            for(var i = 0; i < data.length; i++) {
+                data[i].content = $scope.strip(data[i].content);
+            }
             $scope.articles = data;
+            console.log($scope.articles);
         },
         function(error) {
             //Handle error
             console.log(error);
         }
     );
+
+    $scope.strip = function(html) {
+        var tmp = document.createElement("DIV");
+        tmp.innerHTML = html;
+        return tmp.textContent || tmp.innerText || "";
+    };
 });
