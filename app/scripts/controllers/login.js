e15d30b9137c0973d43f9bc32f1770f162d60dee
Qi <wwqq03@gmail.com>
2014-10-17 10:05:08 +0200

    Implement login controller

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -7,11 +7,18 @@
  * # LoginCtrl
  * Controller of the articleApp
  */
-angular.module('articleApp')
-  .controller('LoginCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('articleControllers')
+.controller('LoginCtrl', function ($scope, $rootScope, settings, $http, $window) {
+    $scope.login = function() {
+        delete $scope.error;
+        $http.post(settings.server_address + '/auth', $scope.form)
+        .success(function(data) {
+            $window.sessionStorage.token = data.token;
+            $rootScope.user = data.profile;
+            $window.location.href = '#/articles';
+        })
+        .error(function(data, status) {
+            $scope.error = data;
+        });
+    };
+});
