f9e7be5b8094c82300bb7a5288ac043eb699e764
Qi <wwqq03@gmail.com>
2014-10-17 12:21:47 +0200

    Fix route and http header

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,6 +3,28 @@ angular.module('articleControllers', []);
 angular.module('articleServices', []);
 var app = angular.module('articleApp', ['articleServices', 'articleControllers','ngAnimate', 'ngCookies', 'ngResource', 'ngRoute', 'ngSanitize', 'ui.bootstrap']);
 
+app.factory('authInterceptor', function ($rootScope, $q, $window) {
+  return {
+    request: function (config) {
+      config.headers = config.headers || {};
+      if ($window.sessionStorage.token) {
+        config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
+      }
+      return config;
+    },
+    response: function (response) {
+      if (response.status === 401) {
+        $window.location.href = '#/login';
+      }
+      return response || $q.when(response);
+    }
+  };
+});
+
+app.config(function ($httpProvider) {
+  $httpProvider.interceptors.push('authInterceptor');
+});
+
 app.config(function ($routeProvider) {
   $routeProvider
     .when('/login', {
@@ -18,6 +40,16 @@ app.config(function ($routeProvider) {
       controller: 'EventsCtrl'
     })
     .otherwise({
-      redirectTo: '/articles'
+      redirectTo: '/login'
     });
+}).
+run(function($rootScope, $location) {
+  $rootScope.$on( "$routeChangeStart", function(event, next, current) {
+    if(!$rootScope.user) {
+      // no logged user, redirect to /login
+      if(next.templateUrl !== "views/login.html") {
+        $location.path("/login");
+      }
+    }
+  });
 });
