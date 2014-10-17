46b6b958422c10ac259bb55116343fd51bc4692f
Qi <wwqq03@gmail.com>
2014-10-17 12:21:01 +0200

    Allow 'Authentication' hearder

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ var supportCORS = function(req, res, next){
     // use "*" here to accept any origin
     res.set('Access-Control-Allow-Origin', '*');
     res.set('Access-Control-Allow-Methods', '*');
-    res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+    res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
     // res.set('Access-Control-Allow-Max-Age', 3600);
     if ('OPTIONS' == req.method) return res.send(200);
     next();
