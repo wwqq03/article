edac894868274fc17253ef199e4c9b5729cd33e4
Qi <wwqq03@gmail.com>
2014-10-19 17:17:19 +0200

    Add API: delete /api/events/:id

--- server/routes/events.js
+++ server/routes/events.js
@@ -6,7 +6,8 @@ var Events = require('../db').EVENTS;
 router.get('/', function(req, res) {
     Events.find(function(err, data) {
         if(err) {
-            res.send(err);
+            res.writeHead(500, err);
+            res.end();
         } else {
             res.json(data);
         }
@@ -16,16 +17,20 @@ router.get('/', function(req, res) {
 router.post('/', function(req, res) {
     var newEvent = req.body;
     if(!newEvent) {
-        res.send(400, 'No request body');
+        res.writeHead(400, 'No request body');
+        res.end();
         return;
     } else if(!newEvent.name) {
-        res.send(400, 'No event name');
+        res.writeHead(400, 'No event name');
+        res.end();
         return;
     } else if(!Array.isArray(newEvent.articles)) {
-        res.send(400, 'Articles must be array');
+        res.writeHead(400, 'Articles must be array');
+        res.end();
         return;
     } else if(newEvent.articles < 2) {
-        res.send(400, 'At least 2 articles');
+        res.writeHead(400, 'At least 2 articles');
+        res.end();
         return;
     }
     var time = Date.now();
@@ -34,11 +39,24 @@ router.post('/', function(req, res) {
     Events.create(newEvent, function(err, doc) {
         if(err) {
             console.log(err);
-            res.send(500, 'Error in storing event');
+            res.writeHead(500, 'Error in storing event');
+            res.end();
             return;
         }
         res.json(doc);
     });
 });
+
+router.delete('/:id', function(req, res) {
+    Events.find({_id: req.params.id}).remove(function(err) {
+        if(err) {
+            res.writeHead(500, err);
+            res.end();
+        } else {
+            res.writeHead(200, {'content-type': 'text/plain'});
+            res.end();
+        }
+    });
+})
 
 module.exports = router;
