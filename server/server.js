4b546ff10bb0c777773de6eb1757cba9e6a6233c
Qi <wwqq03@gmail.com>
2014-10-19 17:16:51 +0200

    Specify cors allowed methods

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ var supportCORS = function(req, res, next){
     if (!req.get('Origin')) return next();
     // use "*" here to accept any origin
     res.set('Access-Control-Allow-Origin', '*');
-    res.set('Access-Control-Allow-Methods', '*');
+    res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, DELETE');
     res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
     // res.set('Access-Control-Allow-Max-Age', 3600);
     if ('OPTIONS' == req.method) return res.send(200);
