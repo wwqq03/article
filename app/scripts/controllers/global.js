8ca7f378cca33dab8dad0155a7cc3675c7f6b215
Qi <wwqq03@gmail.com>
2014-10-19 17:36:35 +0200

    Hide logout button when the user is not logged in

--- app/scripts/controllers/global.js
+++ app/scripts/controllers/global.js
@@ -8,10 +8,19 @@
  * Controller of the articleApp
  */
 angular.module('articleControllers')
-.controller('GlobalCtrl', function ($scope, $window) {
+.controller('GlobalCtrl', function ($scope, $rootScope, $window) {
+    if($window.sessionStorage.token) {
+        $scope.isLoggedIn = true;
+    }
+
+    $rootScope.$on('loggedIn', function() {
+        $scope.isLoggedIn = true;
+    });
+
     $scope.logout = function() {
         delete $window.sessionStorage.token;
         delete $window.sessionStorage.userRole;
+        $scope.isLoggedIn = false;
         $window.location.href = '#/login'
     };
 });
--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -8,13 +8,14 @@
  * Controller of the articleApp
  */
 angular.module('articleControllers')
-.controller('LoginCtrl', function ($scope, settings, $http, $window) {
+.controller('LoginCtrl', function ($scope, settings, $http, $window, $rootScope) {
     $scope.login = function() {
         delete $scope.error;
         $http.post(settings.server_address + '/auth', $scope.form)
         .success(function(data) {
             $window.sessionStorage.token = data.token;
             $window.sessionStorage.userRole = data.profile.role;
+            $rootScope.$broadcast('loggedIn');
             $window.location.href = '#/articles';
         })
         .error(function(data, status) {
