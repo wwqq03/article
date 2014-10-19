095ca9e9aa048768f535adf85ccdbc962b9ab80b
Qi <wwqq03@gmail.com>
2014-10-19 13:42:00 +0200

    Implement event view

--- app/scripts/controllers/events.js
+++ app/scripts/controllers/events.js
@@ -8,10 +8,31 @@
  * Controller of the articleApp
  */
 angular.module('articleControllers')
-  .controller('EventsCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+.controller('EventsCtrl', function ($scope, $window, $q, eventsSvc, articlesSvc) {
+    var userRole = $window.sessionStorage.userRole;
+    if(userRole === 'admin') {
+        $scope.isAdmin = true;
+    }
+
+    $q.all([
+        eventsSvc.get(),
+        articlesSvc.get()
+    ]).then(
+        function(data) {
+            $scope.articles = data[1];
+            $scope.events = data[0];
+        },
+        function(err) {console.log(err);}
+    );
+
+    $scope.loadSlides = function(event) {
+        event.slides = [];
+        angular.forEach(event.articles, function(articleId) {
+            event.slides.push(
+                {
+                    image: $scope.articles[articleId].imgUrl
+                }
+            );
+        });
+    };
+});
